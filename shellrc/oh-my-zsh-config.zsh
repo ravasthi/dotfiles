5076265b5a42994f0651c3c4d5e426c46860b0fc
Richa Avasthi <accounts@richa.xyz>
2021-03-03 09:47:03 -0800

    Refactor functions
    This change is necessary because:
    
    There was no rhyme or reason to the function ordering/grouping, and no clear idea where to add new functions.
    
    This change addresses the need by:
    
    * Functions are now organized by theme (e.g. ones relating to Ruby or Node), then alphabetically within groups.
    * Adding a few aliases to make installing new node versions easier

--- shellrc/oh-my-zsh-config.zsh
+++ shellrc/oh-my-zsh-config.zsh
@@ -41,6 +41,6 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(brew git github git-flow rbenv zsh-syntax-highlighting vi-mode)
+plugins=(brew git github rbenv zsh-syntax-highlighting vi-mode)
 
 source $ZSH/oh-my-zsh.sh
