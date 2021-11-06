a0cd3474bf802bc0d1d515ae6a849dd3ed5c1851
Richa Avasthi <accounts@richa.xyz>
2021-11-06 11:39:39 -0700

    Tweak logic for nvm bash completion initialization.

--- shellrc/nvm-bash-completion.bash
+++ shellrc/nvm-bash-completion.bash
@@ -4,8 +4,6 @@
 
 if [[ -s $HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm ]]; then
   source $HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm
-fi
-
-if [[ -s $NVM_DIR/bash_completion ]]; then
+elif [[ -s $NVM_DIR/bash_completion ]]; then
   source $NVM_DIR/bash_completion
 fi
