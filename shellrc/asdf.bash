61c7d65b30c9f6e926ea0ca6872e20af560da74d
Richa Avasthi <ravasthi@gmail.com>
2022-06-21 13:53:48 -0700

    Don't run asdf plugin commands unless asdf exists

--- shellrc/asdf.bash
+++ shellrc/asdf.bash
@@ -5,10 +5,10 @@
 
 if [[ -s $(brew --prefix asdf)/libexec/asdf.sh ]]; then
   source $(brew --prefix asdf)/libexec/asdf.sh
-fi
 
-# Add plugins if they don't exist
-if ! [[ -d $HOME/.asdf/plugins ]];then
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  # Add plugins if they don't exist
+  if ! [[ -d $HOME/.asdf/plugins ]]; then
+    asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+    asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+  fi
 fi
